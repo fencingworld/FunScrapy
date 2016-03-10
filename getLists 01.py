f4209626914a622ec27481f2341e668dd8da6422
Skyikeli <fencingworld@qq.com>
2016-03-10 15:03:34 +0800

    Update getLists 01.py
    save  all the jsons of year-announcement into database

--- getLists 01.py
+++ getLists 01.py
@@ -17,7 +17,7 @@ sys.setdefaultencoding('utf-8')
 headers = {
 "Host": "www.cninfo.com.cn",
 "Connection":" keep-alive",
-"Content-Length":" 257",
+#"Content-Length":" 257",
 "Accept":" application/json, text/javascript, */*; q=0.01",
 "Origin":" http://www.cninfo.com.cn",
 "X-Requested-With":" XMLHttpRequest",
@@ -42,7 +42,7 @@ fields['category'] 	=  ""
 fields['trade'] 	=  ""      
 fields['column'] 	=  "fund_listed" 
 fields['columnTitle'] = "%E5%9F%BA%E9%87%91%E5%85%AC%E5%91%8A"
-fields['pageNum'] 	=  133			#max = 133
+fields['pageNum'] 	=  1	#max = 133
 fields['pageSize'] 	=  30
 fields['tabName=latest'] = ""
 fields['sortName'] 	=  ""      
@@ -54,7 +54,7 @@ fields['fundtype'] 	=  ""
 fields['seDate'] 	="%E8%AF%B7%E9%80%89%E6%8B%A9%E6%97%A5%E6%9C%9F"
 
 params = urllib.urlencode(fields) 
-print params
+#print params
 
 ###############################################
 # 打开数据库连接
@@ -74,100 +74,108 @@ print "Database version : %s " % data
 # 关闭数据库连接
 
 
-
+countInsert 	= 0
+countRollBack 	= 0
 ###############################################
 DIR = "/cninfo-new/disclosure/fund_listed_latest"
 hostURL = "www.cninfo.com.cn"
 
 conn = httplib.HTTPConnection(hostURL+":80") 
-while (True):
-	
-	conn.request("POST", DIR, params, headers)  
-	response = conn.getresponse()  
-	print response.status, response.reason 
-	
 
-	if response.status==200:
-		break
-	if response.status==307:
-		head = response.getheaders()
-		print head [0][1]
+for i in range(1,135):
+	fields['pageNum'] = i
+	print "get page : %d" % i 
+	params = urllib.urlencode(fields) 
+	while (True):
+		conn.request("POST", DIR, params, headers)  
+		response = conn.getresponse()  
+		print response.status, response.reason 
+		
+		if response.status==200:
+			break
+		if response.status==307:
+			head = response.getheaders()
+			print head [0][1]
 
-		headers["Cookie"] =head[0][1]
-	
-data = response.read() 
-dataDict = json.loads(data)
-#dataList=  dataDict.values()
-#urlBase 	= "http://www.cninfo.com.cn/cninfo-new/disclosure/fund_listed/bulletin_detail/true/"
-urlDownBase = "http://www.cninfo.com.cn/cninfo-new/disclosure/fund_listed/download/"
-dataAncmtList = dataDict["announcements"]
-print type (dataAncmtList)
-print dataAncmtList[0].items()
-#print dataAncmtList 
-print len(dataAncmtList)
-ing =  "My name is %s and weight is %d kg!" % ('Zara', 21) 
-print ing
-for item in dataAncmtList:
-	# 使用cursor()方法获取操作游标 
-	print "################################"
-	print item
-	cursor = db.cursor()
-	# SQL 插入语句
-	sql = """
-		INSERT INTO `funscrapy`.`announcements` (
-		`adjunctSize`, `adjunctType`, `adjunctUrl`, 
-		`announcementContent`, `announcementId`, `announcementTime`, 
-		`announcementTitle`, `announcementType`, `announcementTypeName`, 
-		`associateAnnouncement`, `batchNum`, `columnId`,
-		`id`, `important`, `orgId`, 
-		`pageColumn`, `secCode`, `secName`, 
-		`storageTime`) 
-		VALUES (
-		'%s', '%s', '%s',
-		%s, '%s', '%s',
-		'%s', '%s', %s,
-		%s, '%s', '%s', 
-		%s, '%s', '%s', 
-		'%s', '%s', '%s',
-		'%s')  
-		""" % ( \
-		item["adjunctSize"] , item["adjunctType"] , item["adjunctUrl"] , \
-		"NULL"   , item["announcementId"] , item["announcementTime"] ,\
-		item["announcementTitle"] , item["announcementType"] , "NULL" , \
-		"NULL" , item["batchNum"] , item["columnId"] , \
-		"NULL", 0, item["orgId"] , \
-		item["pageColumn"] , item["secCode"] , item["secName"] , \
-		item["storageTime"] )
-	print "##########==================="
-	sql1 = """
-		INSERT INTO `funscrapy`.`announcements` (
-		`announcementId`, `secCode`, `secName`) 
-		VALUES ('%s','%s','%s')  
-		""" % ( item["announcementId"] , item["secCode"],item["secName"] )
-	print sql
+			headers["Cookie"] =head[0][1]
+		
+	data = response.read() 
+	conn.close()
+	dataDict = json.loads(data)
+	#dataList=  dataDict.values()
+	#urlBase 	= "http://www.cninfo.com.cn/cninfo-new/disclosure/fund_listed/bulletin_detail/true/"
+	urlDownBase = "http://www.cninfo.com.cn/cninfo-new/disclosure/fund_listed/download/"
+	dataAncmtList = dataDict["announcements"]
+	#print type (dataAncmtList)
+	#print dataAncmtList[0].items()
+	#print dataAncmtList 
+	#print len(dataAncmtList)
+	#ing =  "My name is %s and weight is %d kg!" % ('Zara', 21) 
+	#print ing
+	for item in dataAncmtList:
+		# 使用cursor()方法获取操作游标 
+	#	print "################################"
+		#print item
+		#cursor = db.cursor()
+		# SQL 插入语句
+		sql = """
+			INSERT INTO `funscrapy`.`announcements` (
+			`adjunctSize`, `adjunctType`, `adjunctUrl`, 
+			`announcementContent`, `announcementId`, `announcementTime`, 
+			`announcementTitle`, `announcementType`, `announcementTypeName`, 
+			`associateAnnouncement`, `batchNum`, `columnId`,
+			`id`, `important`, `orgId`, 
+			`pageColumn`, `secCode`, `secName`, 
+			`storageTime`) 
+			VALUES (
+			'%s', '%s', '%s',
+			%s, '%s', '%s',
+			'%s', '%s', %s,
+			%s, '%s', '%s', 
+			%s, '%s', '%s', 
+			'%s', '%s', '%s',
+			'%s')  
+			""" % ( \
+			item["adjunctSize"] , item["adjunctType"] , item["adjunctUrl"] , \
+			"NULL"   , item["announcementId"] , item["announcementTime"] ,\
+			item["announcementTitle"] , item["announcementType"] , "NULL" , \
+			"NULL" , item["batchNum"] , item["columnId"] , \
+			"NULL", 0, item["orgId"] , \
+			item["pageColumn"] , item["secCode"] , item["secName"] , \
+			item["storageTime"] )
+	#	print "##########==================="
+		sql1 = """
+			INSERT INTO `funscrapy`.`announcements` (
+			`announcementId`, `secCode`, `secName`) 
+			VALUES ('%s','%s','%s')  
+			""" % ( item["announcementId"] , item["secCode"],item["secName"] )
+		#print sql
 
-	try:
-   # 执行sql语句
-   		cursor.execute(sql)
-   # 提交到数据库执行
-   		db.commit()
-   		print "happy!!!!!!!!!!!!!!!!!!!!!!!!"
-	except Exception, e:
-   # Rollback in case there is any error
-   		print "nimeiaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-   		print e
-   		db.rollback()
-
+		try:
+	   # 执行sql语句
+	   		cursor.execute(sql)
+	   # 提交到数据库执行
+	   		db.commit()
+	   		
+	   		countInsert = countInsert + 1
+		except Exception, e:
+	   # Rollback in case there is any error
+	   		#print "nimeiaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+	   		#print e
+	   		db.rollback()
+	   		countRollBack = countRollBack  + 1
+	print "[countInsert] = %d [countRollBack] = %d " % (countInsert,countRollBack)
+"""
 	setCode = item["secCode"]
 	secName = item["secName"]
 	targetUrl = urlDownBase + item["announcementId"]
 	print setCode+ " " +targetUrl+ " " + secName 
-
+"""
 #urllib.urlretrieve(url, name,cbk)  
 ''''
 score = json.loads(data.decode('UTF-8'))
 print score
 print type(score)
 '''
-conn.close()
+
 db.close()
